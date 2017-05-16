4b4e058746695a1e8c2bd9654b0848643490942d
isbada <isbada@foxmail.com>
2017-05-17 00:43:45 +0800

    第一课面向对象

---
+++ L01面向对象/L01面向对象/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  L01面向对象
+//
+//  Created by fh on 2017/5/17.
+//  Copyright © 2017年 付豪. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
